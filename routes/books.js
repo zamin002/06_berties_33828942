5aa2f7434dfaeb9f5c5b7f4d4969d3f985ae596e
zamin002 <148207180+zamin002@users.noreply.github.com>
2025-12-01 12:27:07 +0000

    error fixes

--- routes/books.js
+++ routes/books.js
@@ -5,7 +5,7 @@ const { check, validationResult } = require('express-validator');
 
 const redirectLogin = (req, res, next) => {
     if (!req.session.userId ) {
-      res.redirect('/users/login') // redirect to the login page
+      res.redirect('../users/login') // redirect to the login page
     } else { 
         next (); // move to the next middleware function
     } 
